45fd123b7ed5e9dabfcbf1adc054203647a97494
Anthoney Hanks <hanksjt@gmail.com>
2022-02-23 20:53:54 -0600

    Initial Load

---
+++ FirstPublisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Threading;
+
+
+namespace FirstPublisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var server = new ResponseSocket())
+            {
+                server.Bind("tcp://*:5555");
+                while(true)
+                {
+                    var message = server.ReceiveFrameString();
+                    Console.WriteLine("Received {0}", message);
+                    // processing the request
+                    Thread.Sleep(100);
+                    Console.WriteLine("Sending World");
+                    server.SendFrame("World");
+                }
+            }
+        }
+    }
+}
